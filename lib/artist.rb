6d77117750e9268a69488ac18bf2e59a0c9fcb00
Michael Kang <michaelkang85@gmail.com>
2018-02-14 00:39:27 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,7 +18,7 @@ class Artist
 
     def add_song_by_name(song)
       new_song = Song.new("#{song}")
-      self.add_song(new_song)
+      self.songs << new_song
       new_song
       @@song_count += 1
     end

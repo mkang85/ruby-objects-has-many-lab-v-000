ec7c926491289b4cc2da2421f4db00170e34f1cc
Michael Kang <michaelkang85@gmail.com>
2018-02-14 00:57:54 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,14 +1,12 @@
 class Author
-  attr_accessor :name
+  attr_accessor :name, :posts
 
   def initialize(name)
     @name = name
     @posts = []
   end
 
-  def posts
-    @posts
-  end
+
 
   def add_post(post)
     @posts << post
